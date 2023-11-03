2b96fa4c3f7d82292d3ab5402257844e44e3a500
pikachu0310 <sunnon@i.softbank.jp>
2023-11-03 23:57:50 +0900

    dropProbability := 0.5

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -9,6 +9,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"os"
 	"os/exec"
@@ -1368,11 +1369,11 @@ var postIsuConditionRequests []PostIsuConditionRequests
 // ISUからのコンディションを受け取る
 func postIsuCondition(c echo.Context) error {
 	// TODO: 一定割合リクエストを落としてしのぐようにしたが、本来は全量さばけるようにすべき
-	//dropProbability := 0.0
-	//if rand.Float64() <= dropProbability {
-	//	c.Logger().Warnf("drop post isu condition request")
-	//	return c.NoContent(http.StatusAccepted)
-	//}
+	dropProbability := 0.5
+	if rand.Float64() <= dropProbability {
+		c.Logger().Warnf("drop post isu condition request")
+		return c.NoContent(http.StatusAccepted)
+	}
 
 	jiaIsuUUID := c.Param("jia_isu_uuid")
 	if jiaIsuUUID == "" {
