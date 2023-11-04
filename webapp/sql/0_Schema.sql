b0d1ff8e1071b7438385d79efd253aa9f06e11e7
pikachu0310 <sunnon@i.softbank.jp>
2023-11-05 00:13:44 +0900

    fix

--- webapp/sql/0_Schema.sql
+++ webapp/sql/0_Schema.sql
@@ -16,6 +16,7 @@ CREATE TABLE `isu` (
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
 CREATE TABLE `isu_condition` (
+  `id` bigint,
   `jia_isu_uuid` CHAR(36) UNIQUE NOT NULL,
   `timestamp` DATETIME NOT NULL,
   `is_sitting` TINYINT(1) NOT NULL,
