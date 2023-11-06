9b39d797dd7d8281aa0342aa831dcf676e5c7b4e
pikachu0310 <sunnon@i.softbank.jp>
2023-11-06 12:02:27 +0900

    IsuGraphをキャッシュ

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -548,6 +548,119 @@ func getIsuByIsuUUID(_ context.Context, isuUUID string) (*Isu, error) {
 	return &isu, nil
 }
 
+var isuGraphByIsuUUID *sc.Cache[string, *[]GraphResponse]
+
+func getIsuGraphByIsuUUID(_ context.Context, isuUUIDAndData string) (*[]GraphResponse, error) {
+	var jiaIsuUUID string
+	var graphDate time.Time
+	// date := time.Unix(datetimeInt64, 0).Truncate(time.Hour).Format("2006-01-02-15")
+	jiaIsuUUID = isuUUIDAndData[:36]
+	graphDate, err := time.Parse("2006-01-02-15", isuUUIDAndData[36:])
+
+	dataPoints := []GraphDataPointWithInfo{}
+	conditionsInThisHour := []IsuCondition{}
+	timestampsInThisHour := []int64{}
+	var startTimeInThisHour time.Time
+	var condition IsuCondition
+
+	rows, err := db.Queryx("SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ? AND `timestamp` <= ? AND ? <= `timestamp` ORDER BY `timestamp` ASC", jiaIsuUUID, graphDate.Add(time.Hour*24), graphDate)
+	if err != nil {
+		return nil, fmt.Errorf("db error: %v", err)
+	}
+
+	for rows.Next() {
+		err = rows.StructScan(&condition)
+		if err != nil {
+			return nil, err
+		}
+
+		truncatedConditionTime := condition.Timestamp.Truncate(time.Hour)
+		if truncatedConditionTime != startTimeInThisHour {
+			if len(conditionsInThisHour) > 0 {
+				data, err := calculateGraphDataPoint(conditionsInThisHour)
+				if err != nil {
+					return nil, err
+				}
+
+				dataPoints = append(dataPoints,
+					GraphDataPointWithInfo{
+						JIAIsuUUID:          jiaIsuUUID,
+						StartAt:             startTimeInThisHour,
+						Data:                data,
+						ConditionTimestamps: timestampsInThisHour})
+			}
+
+			startTimeInThisHour = truncatedConditionTime
+			conditionsInThisHour = []IsuCondition{}
+			timestampsInThisHour = []int64{}
+		}
+		conditionsInThisHour = append(conditionsInThisHour, condition)
+		timestampsInThisHour = append(timestampsInThisHour, condition.Timestamp.Unix())
+	}
+
+	if len(conditionsInThisHour) > 0 {
+		data, err := calculateGraphDataPoint(conditionsInThisHour)
+		if err != nil {
+			return nil, err
+		}
+
+		dataPoints = append(dataPoints,
+			GraphDataPointWithInfo{
+				JIAIsuUUID:          jiaIsuUUID,
+				StartAt:             startTimeInThisHour,
+				Data:                data,
+				ConditionTimestamps: timestampsInThisHour})
+	}
+
+	endTime := graphDate.Add(time.Hour * 24)
+	startIndex := len(dataPoints)
+	endNextIndex := len(dataPoints)
+	for i, graph := range dataPoints {
+		if startIndex == len(dataPoints) && !graph.StartAt.Before(graphDate) {
+			startIndex = i
+		}
+		if endNextIndex == len(dataPoints) && graph.StartAt.After(endTime) {
+			endNextIndex = i
+		}
+	}
+
+	filteredDataPoints := []GraphDataPointWithInfo{}
+	if startIndex < endNextIndex {
+		filteredDataPoints = dataPoints[startIndex:endNextIndex]
+	}
+
+	responseList := []GraphResponse{}
+	index := 0
+	thisTime := graphDate
+
+	for thisTime.Before(graphDate.Add(time.Hour * 24)) {
+		var data *GraphDataPoint
+		timestamps := []int64{}
+
+		if index < len(filteredDataPoints) {
+			dataWithInfo := filteredDataPoints[index]
+
+			if dataWithInfo.StartAt.Equal(thisTime) {
+				data = &dataWithInfo.Data
+				timestamps = dataWithInfo.ConditionTimestamps
+				index++
+			}
+		}
+
+		resp := GraphResponse{
+			StartAt:             thisTime.Unix(),
+			EndAt:               thisTime.Add(time.Hour).Unix(),
+			Data:                data,
+			ConditionTimestamps: timestamps,
+		}
+		responseList = append(responseList, resp)
+
+		thisTime = thisTime.Add(time.Hour)
+	}
+
+	return &responseList, nil
+}
+
 type IsuCache struct {
 	Isu map[string]Isu
 	mu  sync.Mutex
@@ -1100,11 +1213,19 @@ func getIsuGraph(c echo.Context) error {
 		return c.String(http.StatusNotFound, "not found: isu")
 	}
 
-	res, err := generateIsuGraphResponse(jiaIsuUUID, date)
+	dataString := date.Format("2006-01-02-15")
+	isuGraphByIsuUUID.Get(context.Background(), jiaIsuUUID+dataString)
 	if err != nil {
 		c.Logger().Error(err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
+	resPointer, err := isuGraphByIsuUUID.Get(context.Background(), jiaIsuUUID+dataString)
+	res := *resPointer
+	//res, err := generateIsuGraphResponse(jiaIsuUUID, date)
+	//if err != nil {
+	//	c.Logger().Error(err)
+	//	return c.NoContent(http.StatusInternalServerError)
+	//}
 
 	if err != nil {
 		c.Logger().Errorf("db error: %v", err)
