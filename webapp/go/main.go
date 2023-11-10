a2d368dea4c288b7068de5c84be96063028fbc41
pikachu0310 <sunnon@i.softbank.jp>
2023-11-10 23:46:55 +0900

    15

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -343,7 +343,7 @@ func jsonEncode(res any) []byte {
 }
 
 func updateTrend() {
-	if time.Since(benchTime) > 30*time.Second {
+	if time.Since(benchTime) > 15*time.Second {
 		return
 	}
 	var isuList []Isu
